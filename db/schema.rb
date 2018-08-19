8361892e5a650fe30e206a5d3a600a2af587df06
Mehwish Aziz <mehwisha@msn.com>
2018-08-19 03:04:29 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 9) do
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.integer "show_id"
+    t.integer "actor_id"
+    t.string "catchphrase"
+  end
 
   create_table "networks", force: :cascade do |t|
     t.string "call_letters"
@@ -20,6 +27,9 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "shows", force: :cascade do |t|
     t.string "name"
     t.integer "network_id"
+    t.string "genre"
+    t.string "day"
+    t.string "season"
   end
 
 end
